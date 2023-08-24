339dedce8a8aea18afdddb33df77b682f3513861
Mario A <mac132@live.com.mx>
2023-08-23 20:26:39 -0600

    [Add] project start

---
+++ src/test/java/com/marketplatzi/platziMarket/PlatziMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marketplatzi.platziMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatziMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
