05aea344e89e9d50cefd3b6e3a1628d74a39a017
Mario A <mac132@live.com.mx>
2023-09-10 23:09:26 -0600

    [Update] add entities of BD and connection with the BD

---
+++ src/main/java/com/marketplatzi/platziMarket/Persistence/Entities/Clientes.java
@@ -0,0 +1,20 @@
+package com.marketplatzi.platziMarket.Persistence.Entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "clientes")
+public class Clientes {
+
+    @Id
+    private String id;
+    private String nombre;
+    private String apellidos;
+    private Integer celular;
+    private String direccion;
+    @Column(name = "correo_electronico")
+    private String email;
+}
