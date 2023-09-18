ffbedd0994aad6def98c4a1890f35ac8d44cc76a
Mario A <mac132@live.com.mx>
2023-09-17 19:20:17 -0600

    [Update] Finish entities

--- src/main/java/com/marketplatzi/platziMarket/Persistence/Entities/Clientes.java
+++ src/main/java/com/marketplatzi/platziMarket/Persistence/Entities/Clientes.java
@@ -17,4 +17,52 @@ public class Clientes {
     private String direccion;
     @Column(name = "correo_electronico")
     private String email;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public Integer getCelular() {
+        return celular;
+    }
+
+    public void setCelular(Integer celular) {
+        this.celular = celular;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
